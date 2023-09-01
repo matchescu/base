8d24de37c081a709d42a9a4fe05f11a9be77689b
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-09-01 14:36:16 +0300

    doc: add citation for source of algorithm

--- src/matchescu/common/partitioning.py
+++ src/matchescu/common/partitioning.py
@@ -1,6 +1,8 @@
 def compute_partition(a: list, equivalence: set[tuple]):
     """Partition an input set according to an equivalence relation.
 
+    The algorithm is available `here<https://doi.org/10.1007/978-3-319-21840-3_36>`_.
+
     :param partition: buffer variable that contains the output
     :param a: input set of items to partition
     :param equivalence: pairs that define which items are equivalent to other items
