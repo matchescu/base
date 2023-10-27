0f527134ab3008300eb1053eb040f2a0e25d7c54
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-10-27 11:46:37 +0300

    make partitioning code work with lists instead of sets

--- src/matchescu/common/partitioning.py
+++ src/matchescu/common/partitioning.py
@@ -1,27 +1,32 @@
 from networkx import Graph, connected_components
 
 
-def compute_partition(a: list, equivalence: set[tuple]) -> frozenset[frozenset[tuple]]:
+def compute_partition(all_references: list, equivalence: list[tuple]) -> list[list[tuple]]:
     """Partition an input set according to an equivalence relation.
 
-    The algorithm is available `here<https://doi.org/10.1007/978-3-319-21840-3_36>`_.
+    We use Graphs to find the clusters. Clusters are just the connected
+    components of a graph built from an input of pairs.
 
-    :param partition: buffer variable that contains the output
-    :param a: input set of items to partition
+    :param all_references: all entity references that make up the input domain
     :param equivalence: pairs that define which items are equivalent to other items
+    :return: a list of lists of tuples, where:
+
+    * each tuple is an entity reference
+    * tuples are grouped in clusters which are represented as lists
+    * the final result is a list of clusters
     """
     g = Graph(equivalence)
     clusters = []
     all_matching = set()
 
     for cluster in connected_components(g):
-        clusters.append(frozenset(cluster))
+        clusters.append(cluster)
         for item in cluster:
             all_matching.add(item)
 
-    for item in a:
+    for item in all_references:
         if item in all_matching:
             continue
         clusters.append(frozenset([item]))
 
-    return frozenset(clusters)
+    return clusters
