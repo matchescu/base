190f8ef496451dcf39ad74632f952d01464c1e7c
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-11-10 22:15:52 +0200

    fix: implement partitioning using union-find

--- src/matchescu/common/partitioning.py
+++ src/matchescu/common/partitioning.py
@@ -1,11 +1,55 @@
-from networkx import Graph, connected_components
+from typing import TypeVar, Hashable, Generic
+
+
+T = TypeVar("T", bound=Hashable)
+
+
+class EquivalenceClassPartitioner(Generic[T]):
+    def __init__(self, items: list[T]):
+        self._rank = {item: 0 for item in items}
+        self._parent = {item: item for item in items}
+        self._items = items
+
+    def _find(self, x: T) -> T:
+        if self._parent[x] == x:
+            return x
+        # path compression
+        self._parent[x] = self._find(self._parent[x])
+        return self._parent[x]
+
+    def _union(self, x: T, y: T) -> None:
+        x_root = self._find(x)
+        y_root = self._find(y)
+
+        if x_root == y_root:
+            return
+
+        if self._rank[x_root] < self._rank[y_root]:
+            self._parent[x_root] = y_root
+        elif self._rank[y_root] < self._rank[x_root]:
+            self._parent[y_root] = x_root
+        else:
+            # does not matter which goes where
+            # make sure we increase the correct rank
+            self._parent[y_root] = x_root
+            self._rank[x_root] += 1
+
+    def __call__(self, pairs: list[tuple[T, T]]) -> list[list[T]]:
+        for x, y in pairs:
+            self._union(x, y)
+        classes = {item: dict() for item in self._items}
+        abc = dict()
+        for item in self._items:
+            classes[self._find(item)][item] = None
+        return [
+            list(eq_class) for eq_class in classes.values() if len(eq_class) > 0
+        ]
 
 
 def compute_partition(all_references: list, equivalence: list[tuple]) -> list[list[tuple]]:
     """Partition an input set according to an equivalence relation.
 
-    We use Graphs to find the clusters. Clusters are just the connected
-    components of a graph built from an input of pairs.
+    The algorithm uses the Union-Find or DFU data structure.
 
     :param all_references: all entity references that make up the input domain
     :param equivalence: pairs that define which items are equivalent to other items
@@ -15,18 +59,6 @@ def compute_partition(all_references: list, equivalence: list[tuple]) -> list[li
     * tuples are grouped in clusters which are represented as lists
     * the final result is a list of clusters
     """
-    g = Graph(equivalence)
-    clusters = []
-    all_matching = set()
-
-    for cluster in connected_components(g):
-        clusters.append(cluster)
-        for item in cluster:
-            all_matching.add(item)
-
-    for item in all_references:
-        if item in all_matching:
-            continue
-        clusters.append(frozenset([item]))
-
-    return clusters
+    partitioner = EquivalenceClassPartitioner(all_references)
+    result = partitioner(equivalence)
+    return result
--- tests/matchescu/common/test_partitioning.py
+++ tests/matchescu/common/test_partitioning.py
@@ -2,51 +2,51 @@ from matchescu.common.partitioning import compute_partition
 
 
 def test_reflexivity():
-    partition = compute_partition(["a"], {("a", "a")})
+    partition = compute_partition(["a"], [("a", "a")])
     assert len(partition) == 1
-    assert partition == frozenset([frozenset(["a"])])
+    assert partition == [["a"]]
 
 
 def test_symmetry():
-    partition = compute_partition(["a", "b"], {("a", "b"), ("b", "a")})
+    partition = compute_partition(["a", "b"], [("a", "b"), ("b", "a")])
     assert len(partition) == 1
-    assert partition == frozenset([frozenset(["a", "b"])])
+    assert partition == [["a", "b"]]
 
 
 def test_transitivity():
     partition = compute_partition(
         ["a", "b", "c"],
-        {
+        [
             ("a", "b"),
             ("b", "c"),
-        }
+        ]
     )
 
     assert len(partition) == 1
-    assert partition == frozenset([frozenset(["a", "b", "c"])])
+    assert partition == [["a", "b", "c"]]
 
 
 def test_create_single_set():
     partition = compute_partition(
         ["a", "b", "c", "d"],
-        {
+        [
             ("a", "b"),
             ("b", "c"),
             ("d", "a")
-        }
+        ]
     )
 
     assert len(partition) == 1
-    assert partition == frozenset([frozenset(["a", "b", "c", "d"])])
+    assert partition == [["a", "b", "c", "d"]]
 
 
 def test_isolated_item():
     partition = compute_partition(
         ["a", "b", "c", "d"],
-        {
+        [
             ("a", "b"),
             ("b", "c"),
-        }
+        ]
     )
     assert len(partition) == 2
-    assert partition == frozenset([frozenset(["a", "b", "c"]), frozenset(["d"])])
+    assert partition == [["a", "b", "c"], ["d"]]
