dfcdf356322a65d47560b40aca3a99613d8ee0aa
Andrei Olar <andrei.olar@gmail.com>
2024-10-02 15:46:39 +0300

    refactor code to support fellegi-sunter experiment

--- src/matchescu/similarity/__init__.py
+++
@@ -1,3 +0,0 @@
-from matchescu.similarity._common import Similarity, MatchResult, ValueSimilarityMatch
-from matchescu.similarity._exact_match import ExactMatch
-from matchescu.similarity._string import StringSimilarity, Jaccard, Jaro, JaroWinkler, Levenshtein
---
+++ src/matchescu/typing/__init__.py
@@ -0,0 +1,6 @@
+from matchescu.typing._callable import EntityReferenceIdentifier
+from matchescu.typing._data import Record
+from matchescu.typing._entity_resolution import EntityReference, EntityProfile
+
+
+__all__ = ["EntityReference", "EntityReferenceIdentifier", "EntityProfile", "Record"]
